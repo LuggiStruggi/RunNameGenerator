52615f23e54128c226c4ef9af0b435157d992394
LuggiStruggi <lukasmkoenig@gmx.net>
2022-02-03 15:18:48 +0100

    updates

--- runnamegen/__init__.py
+++ runnamegen/__init__.py
@@ -1,6 +1,7 @@
 import os
 from functools import reduce
 import random
+import json
 
 __version__ = '1.6.2'
 
@@ -10,8 +11,37 @@ def listdir_fullpath(d):
 def relative_path(d):
 	return os.path.join(os.path.dirname(os.path.abspath(__file__)), d)
 
-def generate(delimiter:str = " "):
+def read_config(name):
+	with open(relative_path('config.json')) as f:
+		config = json.load(f)[name]
+	return config
+
+def generate(delimiter: str = " "):
+	
+	alliteration = read_config("alliteration")
+	letter = read_config("alphabet")
+	
 	excludes = [relative_path(os.path.join("nouns", line.strip()+".txt")) for line in open(relative_path("excludes.txt"))]
-	adjectives = reduce(lambda x, y : x+y, [[line.strip() for line in open(filename)] for filename in listdir_fullpath(relative_path("adjectives"))])
-	nouns = reduce(lambda x, y : x+y, [[line.strip() for line in open(filename)] for filename in listdir_fullpath(relative_path("nouns")) if filename not in excludes])
-	return random.choice(adjectives) + delimiter + random.choice(nouns)
+
+	# nouns
+	nouns = [[line.strip() for line in open(filename)] for filename in listdir_fullpath(relative_path("nouns")) if filename not in excludes]
+	nouns = reduce(lambda x, y : x+y, nouns)
+	if nouns == []:
+		nouns = ["john-doe"]
+	if letter != "":
+		nouns = [n for n in nouns if n[0] == letter]
+	if nouns == []:
+		nouns = ["john-doe"]
+	random_noun = random.choice(nouns)
+	
+	#adjectives
+	adjectives = [[line.strip() for line in open(filename)] for filename in listdir_fullpath(relative_path("adjectives"))]
+	adjectives = reduce(lambda x, y : x+y, adjectives)
+	if adjectives == []:
+		adjectives = ["standard"]
+	if alliteration:
+		adjectives = [a for a in adjectives if a[0] == random_noun[0]]
+	if adjectives == []:
+		adjectives = ["standard"]
+
+	return random.choice(adjectives) + delimiter + random_noun
--- runnamegen/commands.py
+++ runnamegen/commands.py
@@ -20,6 +20,30 @@ def cmd_exclude(category):
 	with open(relative_path("excludes.txt"), 'w') as new:
 		new.writelines(sorted(excludes))
 
+def change_config(name, state):
+	with open(relative_path('config.json')) as f:
+		config = json.load(f)
+	config[name] = state
+	with open(relative_path('config.json'), 'w') as f:
+		json.dump(config, f)
+
+def read_config(name):
+	with open(relative_path('config.json')) as f:
+		config = json.load(f)[name]
+	return config
+
+def increment_alphabet():
+	with open(relative_path('config.json')) as f:
+		config = json.load(f)
+	if config["alphabet"] == "":
+		return
+	elif config["alphabet"] == "z":
+		config["alphabet"] = "a"
+	else:	
+		config["alphabet"] = chr(ord(config["alphabet"][0]) + 1)
+	with open(relative_path('config.json'), 'w') as f:
+		json.dump(config, f)
+
 def include_choices():
 	return [line.strip().lower() for line in open(relative_path("excludes.txt"))]
 
@@ -33,16 +57,47 @@ def cmd_include(category):
 	with open(relative_path("excludes.txt"), 'w') as new:
 		new.writelines(sorted(excludes))
 
-def cmd_generate(delimiter: str=" ", alliteration=False):
-	print(generate(delimiter, alliteration, letter='b'))
+def cmd_generate(delimiter: str = " "):
+	print(generate(delimiter))
+
+def delete_word_from_file(word, filename):
+	words = [line for line in open(filename) if line != word and line != word+"\n"]
+	with open(filename, 'w') as new:
+		new.writelines(sorted(words))
+
+def handle_duplicates(previous_words: dict, current_words: list, filename: str) -> dict:
+	for w in current_words:
+		if w in previous_words:
+			done = False
+			inpt = input(f"File [1]: {os.path.splitext(previous_words[w])[0]} and\n"+
+                         f"File [2]: {os.path.splitext(filename)[0]} share the word {w}.\n"+
+						 f"Would you like the word removed from [1], [2] or leave it in both [3]?")
+			while not done:
+				if inpt == "1":
+					delete_word_from_file(w, previous_words[w])
+					done = True
+				elif inpt == "2":
+					current_words.remove(w)
+					done = True
+				elif inpt == "3":
+					done = True
+				else:
+					inpt = input("Type either 1, 2 or 3.")
+					done = False		
+		else:
+			previous_words[w] = filename
+
+	return previous_words, current_words
 
 def cmd_cleanup():
+	prev_words = {}
 	for path in ["adjectives", "nouns"]:
 		for filename in listdir_fullpath(relative_path(path)):
 			words = [line.lower() for line in open(filename) if len(line.strip().split(" ")) == 1]
 			words = list(set(words))
-			with open(filename, 'w') as new:
-				new.writelines(sorted(words))
+			prev_words, words = handle_duplicates(prev_words, words, filename)
+		with open(filename, 'w') as new:
+			new.writelines(sorted(words))
 
 def cmd_add_nouns(filename):
 	noun_dir = relative_path("nouns")
@@ -74,8 +129,15 @@ def cmd_main():
 	p_generate.add_argument('-n', type=int, help='number of random words', default=1)
 	
 	p_cleanup = subparsers.add_parser('cleanup', help='clean up word files (remove 2 word words and lowercase)')
+
+	p_alphabet = subparsers.add_parser('alphabet', help='alphabet mode')
+	p_alphabet.add_argument('mode', type=str, choices=['on', 'off'], help='alphabet mode on or off')
 	
+	p_alliteration = subparsers.add_parser('alliteration', help='alliteration mode')
+	p_alliteration.add_argument('mode', type=str, choices=['on', 'off'], help='alliteration mode on or off')
+
 	args = parser.parse_args()
+
 	if args.subcommand == "exclude":
 		cmd_exclude(args.category)
 	elif args.subcommand == "include":
@@ -85,6 +147,16 @@ def cmd_main():
 	elif args.subcommand == "add-adjectives":
 		cmd_add_nouns(args.filename)
 	elif args.subcommand == "generate":
-		[cmd_generate(args.delimiter, alliteration=True) for i in range(args.n)]
+		[(cmd_generate(args.delimiter), increment_alphabet()) for i in range(args.n)]
 	elif args.subcommand == "cleanup":
 		cmd_cleanup()
+	elif args.subcommand == "alliteration":
+		if args.mode == "on":
+			change_config("alliteration", True)
+		elif args.mode == "off":
+			change_config("alliteration", False)
+	elif args.subcommand == "alphabet":
+		if args.mode == "on":
+			change_config("alphabet", "a")
+		elif args.mode == "off":
+			change_config("alphabet", "")
